59d47e80fee0600a4b02c56dd480f7689a2a5c68
Weidong Sun <sunweidong90@gmail.com>
2020-07-19 12:15:32 -0500

    Updates

---
+++ precache-manifest.2423ce0c9927fc201640b40f8d297cfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521bdbf027923f4cedf0ca4a368fa312",
+    "url": "/uiuc-ds-narrative-vis/index.html"
+  },
+  {
+    "revision": "796d1e74da01936bf1a9",
+    "url": "/uiuc-ds-narrative-vis/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d69ce3c531a80483d3ae",
+    "url": "/uiuc-ds-narrative-vis/static/js/2.67f605d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uiuc-ds-narrative-vis/static/js/2.67f605d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796d1e74da01936bf1a9",
+    "url": "/uiuc-ds-narrative-vis/static/js/main.81132c94.chunk.js"
+  },
+  {
+    "revision": "4791f6b976ae1c4da817",
+    "url": "/uiuc-ds-narrative-vis/static/js/runtime-main.17a6ae07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uiuc-ds-narrative-vis/static/media/logo.5d5d9eef.svg"
+  }
+]);
